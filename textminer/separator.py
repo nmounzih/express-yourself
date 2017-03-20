58685bc508a55d7e3a381873591e6ebd1013bb55
Nadia Mounzih <nmounzih@yahoo.com>
2017-03-20 16:30:08 -0400

    Made 'phone_number' functino work but too much python logic

--- textminer/separator.py
+++ textminer/separator.py
@@ -6,3 +6,19 @@ def words(text):
         return
     else:
         return re.findall(r'[0-9-A-za-z]+', text)
+
+
+def phone_number(text):
+    phone_dict = {}
+    split_list = re.findall(r'[0-9]+', text)
+    if len(split_list) == 3:
+        phone_dict['area_code'] = split_list[0]
+        phone_dict['number'] = split_list[1] + '-' + split_list[2]
+        return phone_dict
+    elif len(split_list) == 2:
+        return None
+    elif len(split_list) == 1:
+        for item in split_list:
+            phone_dict['area_code'] = item[:3]
+            phone_dict['number'] = item[3:6] + '-' + item[6:]
+        return phone_dict
