358c5834930bd13d29f65524afbdbccf04d03e78
Nadia Mounzih <nmounzih@yahoo.com>
2017-03-21 10:55:00 -0400

    Fixed functions

--- textminer/validator.py
+++ textminer/validator.py
@@ -2,41 +2,38 @@ import re
 
 
 def binary(number):
-    return re.findall(r'[0-1]+', number)
+    return re.search(r'^(0|1[01]*)$', number)
 
 
 def binary_even(number):
-    return re.findall(r'^[0-1]{0,16}[0]$', number)
+    return re.search(r'^(0|1[01]*0)$', number)
 
 
 def hex(text):
-    if len(re.findall(r'[0-9A-Fa-f]+', text)) != 1:
-        return False
-    else:
-        return True
+    return re.search(r'^[0-9A-Fa-f]+$', text)
 
 
 def word(text):
-    if len(re.findall(r'[A-Z-a-z]+', text)) != 1:
-        return False
-    else:
-        return True
+    return re.search(r'^\b[\w-]*[A-Za-z]+\b$', text)
 
 
 def words(text, count=None):
-    return re.findall(r'[A-Z-a-z^!]+', text)
+    result = re.findall(r'\b[\w-]*[A-Za-z]+\b', text)
+    return len(result) == count if count else result
 
 
 def phone_number(number):
     return re.findall(r'[(9][\d]+', number)
+    #return re.search(r'\(?[0-9]{3}\)?[\D]?[0-9]{3}[\D]?[0-9]{4}', number)
 
 
 def money(number):
-    return re.findall(r'^[$][\d,.]+', number)
+    return re.search(r'^\$[0-9]{1,3}?(\,?[0-9]{3}*)(\.[0-9]{2})?$', number)
 
 
 def zipcode(number):
     return re.findall(r'^[0-9]{5}(?:-[0-9]{4})?$', number)
+    #return re.search(r'^[0-9]{5}(-[0-9]{4})?$', string)
 
 
 def date(number):
