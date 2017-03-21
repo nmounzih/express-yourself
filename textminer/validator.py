7347d66457783e242c6ea9b4a7fcb168a3abd70b
Nadia Mounzih <nmounzih@yahoo.com>
2017-03-20 20:15:43 -0400

    Added phone number function

--- textminer/validator.py
+++ textminer/validator.py
@@ -16,4 +16,24 @@ def hex(text):
         return True
 
 
-def word():
+def word(text):
+    if len(re.findall(r'[A-Z-a-z]+', text)) != 1:
+        return False
+    else:
+        return True
+
+
+# def words(text, count=None):
+#     if count:
+#         if re.findall(r'[A-Z-a-z]+', text) != count:
+#             return False
+#     else:
+#         return re.findall(r'[A-Z-a-z]+', text)
+
+
+def phone_number(number):
+    return re.findall(r'[(9][\d]+', number)
+
+
+def money(number):
+    return re.findall(r'^[$][\d]+', number) or re.findall(r'^[$][\d]+[.][\d]+', number)
